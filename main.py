bb832b631617f1c0f17307456aa135f62c6a3dfc
netcore-vishal <netcore.vishal@gmail.com>
2025-01-07 15:57:38 +0530

    Initial setup

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+from dotenv import load_dotenv
+import os
+from groq import Groq
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.duckduckgo import DuckDuckGo
+
+load_dotenv()
+
+web_agent = Agent(
+    name="Web Agent",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
 if __name__ == "__main__":
-    print('Init')
+    web_agent.print_response("Who are you?")
